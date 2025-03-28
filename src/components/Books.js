bf63820e7947925ab83ee7f6e9940ee9f3f90427
Michael <mikek222@gmail.com>
2025-03-28 16:42:45 -0400

    completed search feature

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,22 @@
 import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import { BooksContext } from "../context/BooksContext";
 
 export default function Books() {
+  const query = new URLSearchParams(useLocation().search);
+  const search = query.get('search') || ''
   const books = useContext(BooksContext);
+
+  const filteredBooks = books;
 
   return (
     <div>
       <h1>All Books</h1>
       <ul>
-        {books.map((book) => (
-          <li key={book.id}>{book.title} by {book.author}</li>
+        {filteredBooks.map((book) => (
+          <li key={book.id}>
+            {book.title} by {book.author}
+          </li>
         ))}
       </ul>
     </div>
